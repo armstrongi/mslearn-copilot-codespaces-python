30dccbdac04b75b7cc17bda61048c193cc94bb29
Ian Armstrong <50608249+armstrongi@users.noreply.github.com>
2024-08-28 03:59:58 +0000

    Create new endpoint

--- webapp/main.py
+++ webapp/main.py
@@ -27,7 +27,8 @@ def root():
 @app.post('/generate')
 def generate(body: Body):
     """
-    Generate a pseudo-random token ID of twenty characters by default. Example POST request body:
+    Generate a pseudo-random token ID of twenty
+    characters by default. Example POST request body:
 
     {
         "length": 20
@@ -35,3 +36,23 @@ def generate(body: Body):
     """
     string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
     return {'token': string}
+
+
+class Text(BaseModel):
+    text: str
+
+# Create a FastAPI endpoint that accepts a POST request
+# with a JSON body containing a single field called "text"
+# and returns a checksum of the text
+
+
+@app.post('/checksum')
+def checksum(text: Text):
+    """
+    Generate a checksum of the provided text. Example POST request body:
+
+    {
+        "text": "Hello, World!"
+    }
+    """
+    return {'checksum': hash(text.text)}
